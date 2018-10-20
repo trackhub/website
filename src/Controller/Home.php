b79246ca0beca626cc98c2d3758a76fd9838ce44
po_taka <angel.koilov@gmail.com>
2018-10-20 21:03:45 +0300

    gps finder

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Gps\Point;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -20,6 +21,51 @@ class Home extends AbstractController
             [
                 'gpsCollection' => $gpsCollection,
             ]
+        );
+    }
+
+    public function find($neLat, $neLon, $swLat, $swLon, int $size)
+    {
+        $doctrine = $this->getDoctrine();
+        $em = $doctrine->getRepository(\App\Entity\Gps::class);
+        /* @var $em EntityRepository */
+
+        $responseData = [];
+
+        for ($i = 0; $i < $size; $i++) {
+            $qb = $em->createQueryBuilder('g');
+            $qb->select('count(g.id)');
+            $q = $qb->getQuery();
+            $data = $q->getSingleResult();
+            $count = current($data);
+            if ($count > 10) {
+                // @TODO add "zoom please"
+            } else {
+                $qb = $em->createQueryBuilder('g');
+                $qb->select('g');
+                $q = $qb->getQuery();
+                $qResult = $q->getResult();
+                /* @var $qResult \App\Entity\Gps[] */
+                foreach($qResult as $gps) {
+                    $gpsArrayData = [];
+                    $gpsArrayData['id'] = $gps->getId();
+                    foreach ($gps->getPoints() as $point) {
+                        /* @var $point Point */
+                        $gpsArrayData['pints'][] = [
+                            'lat' => $point->getLat(),
+                            'lon' => $point->getLon(),
+                        ];
+                    }
+
+                    $responseData[] = $gpsArrayData;
+                }
+            }
+        }
+
+
+
+        return new Response(
+            json_encode($responseData)
         );
     }
 }
