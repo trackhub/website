22724c43519f86e885c57d821e2fe280f0cfa638
po_taka <angel.koilov@gmail.com>
2018-12-01 19:07:12 +0200

    bugfixes in gps search + packages update

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Gps\Point;
 use Doctrine\ORM\EntityRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -18,56 +19,75 @@ class Home extends AbstractController
         );
     }
 
-    public function find($neLat, $neLon, $swLat, $swLon, int $size)
+    public function find($neLat, $neLon, $swLat, $swLon, Request $request)
     {
         $doctrine = $this->getDoctrine();
         $em = $doctrine->getRepository(\App\Entity\Gps::class);
         /* @var $em EntityRepository */
 
-        $responseData = [];
+        $qb = $em->createQueryBuilder('g');
+        $qb->select('count(g.id)');
 
-        for ($i = 0; $i < $size; $i++) {
+        $qb->andWhere(
+            $qb->expr()->andX(
+                $qb->expr()->lte('g.pointNorthEastLat', $neLat),
+                $qb->expr()->gte('g.pointSouthWestLat', $swLat),
+                $qb->expr()->lte('g.pointNorthEastLng', $neLon),
+                $qb->expr()->gte('g.pointSouthWestLng', $swLon)
+            )
+        );
+        $q = $qb->getQuery();
+        $data = $q->getSingleResult();
+        $count = current($data);
+
+        if ($count > 10) {
+            // status 2 = too many tracks found
+            return new Response(
+                json_encode([
+                    'status' => 2,
+                ]),
+                Response::HTTP_OK,
+                [
+                    'Content-Type' => 'text/json',
+                ]
+            );
+        } else {
             $qb = $em->createQueryBuilder('g');
-            $qb->select('count(g.id)');
             $qb->andWhere(
                 $qb->expr()->andX(
                     $qb->expr()->lte('g.pointNorthEastLat', $neLat),
-                    $qb->expr()->gte('g.pointNorthEastLng', $neLon),
                     $qb->expr()->gte('g.pointSouthWestLat', $swLat),
-                    $qb->expr()->lte('g.pointSouthWestLng', $swLon)
+                    $qb->expr()->lte('g.pointNorthEastLng', $neLon),
+                    $qb->expr()->gte('g.pointSouthWestLng', $swLon)
                 )
             );
+            $qb->select('g');
             $q = $qb->getQuery();
-            $data = $q->getSingleResult();
-            $count = current($data);
-            if ($count > 10) {
-                // @TODO add "zoom please"
-            } else {
-                $qb = $em->createQueryBuilder('g');
-                $qb->select('g');
-                $q = $qb->getQuery();
-                $qResult = $q->getResult();
-                /* @var $qResult \App\Entity\Gps[] */
-                foreach($qResult as $gps) {
-                    $gpsArrayData = [];
-                    $gpsArrayData['id'] = $gps->getId();
-                    foreach ($gps->getOptimizedPoints() as $point) {
-                        /* @var $point Point */
-                        $gpsArrayData['points'][] = [
-                            'lat' => $point->getLat(),
-                            'lng' => $point->getLng(),
-                        ];
-                    }
+            $qResult = $q->getResult();
+            /* @var $qResult \App\Entity\Gps[] */
 
-                    $responseData[] = $gpsArrayData;
+            $responseData = [];
+
+            foreach($qResult as $gps) {
+                $gpsArrayData = [];
+                $gpsArrayData['id'] = $gps->getId();
+                foreach ($gps->getOptimizedPoints() as $point) {
+                    /* @var $point Point */
+                    $gpsArrayData['points'][] = [
+                        'lat' => $point->getLat(),
+                        'lng' => $point->getLng(),
+                    ];
                 }
+
+                $responseData[] = $gpsArrayData;
             }
         }
 
-
-
         return new Response(
-            json_encode($responseData),
+            json_encode([
+                'status' => 1,
+                'data' => $responseData,
+            ]),
             Response::HTTP_OK,
             [
                 'Content-Type' => 'text/json',
