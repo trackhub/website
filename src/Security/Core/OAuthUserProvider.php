305120617a7d11b23fb8a5bd26d70fc26ed5a674
po_taka <angel.koilov@gmail.com>
2019-01-27 22:58:02 +0200

    code optimization

--- src/Security/Core/OAuthUserProvider.php
+++ src/Security/Core/OAuthUserProvider.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use HWI\Bundle\OAuthBundle\Connect\AccountConnectorInterface;
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider;
 use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthAwareUserProviderInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use FOS\UserBundle\Model\User;
@@ -18,24 +19,14 @@ use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-class OAuthUserProvider implements UserProviderInterface, AccountConnectorInterface, OAuthAwareUserProviderInterface
+class OAuthUserProvider extends FOSUBUserProvider
 {
-    /**
-     * @var UserManagerInterface
-     */
-    protected $userManager;
-
     /**
      * @var array
      */
     protected $properties = array(
         'identifier' => 'id',
     );
-
-    /**
-     * @var PropertyAccessor
-     */
-    protected $accessor;
 
     private $entityManager;
 
@@ -51,19 +42,6 @@ class OAuthUserProvider implements UserProviderInterface, AccountConnectorInterf
         $this->userManager = $userManager;
         $this->properties = array_merge($this->properties, $properties);
         $this->accessor = PropertyAccess::createPropertyAccessor();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function loadUserByUsername($username)
-    {
-        // Compatibility with FOSUserBundle < 2.0
-        if (class_exists('FOS\UserBundle\Form\Handler\RegistrationFormHandler')) {
-            return $this->userManager->loadUserByUsername($username);
-        }
-
-        return $this->userManager->findUserByUsername($username);
     }
 
     /**
@@ -90,101 +68,9 @@ class OAuthUserProvider implements UserProviderInterface, AccountConnectorInterf
             $this->entityManager->persist($user);
             $this->entityManager->flush();
 
-            $user = $this->loadUserByUsername($username);
+            $user = $this->loadUserByUsername($response->getNickname());
         }
 
         return $user;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function connect(UserInterface $user, UserResponseInterface $response)
-    {
-        if (!$user instanceof User) {
-            throw new UnsupportedUserException(sprintf('Expected an instance of FOS\UserBundle\Model\User, but got "%s".', get_class($user)));
-        }
-
-        $property = $this->getProperty($response);
-        $username = $response->getUsername();
-
-        if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
-            $this->disconnect($previousUser, $response);
-        }
-
-        if ($this->accessor->isWritable($user, $property)) {
-            $this->accessor->setValue($user, $property, $username);
-        } else {
-            throw new \RuntimeException(sprintf('Could not determine access type for property "%s".', $property));
-        }
-
-        $this->userManager->updateUser($user);
-    }
-
-    /**
-     * Disconnects a user.
-     *
-     * @param UserInterface $user
-     * @param UserResponseInterface $response
-     */
-    public function disconnect(UserInterface $user, UserResponseInterface $response)
-    {
-        $property = $this->getProperty($response);
-
-        $this->accessor->setValue($user, $property, null);
-        $this->userManager->updateUser($user);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function refreshUser(UserInterface $user)
-    {
-        // Compatibility with FOSUserBundle < 2.0
-        if (class_exists('FOS\UserBundle\Form\Handler\RegistrationFormHandler')) {
-            return $this->userManager->refreshUser($user);
-        }
-
-        $identifier = $this->properties['identifier'];
-        if (!$user instanceof User || !$this->accessor->isReadable($user, $identifier)) {
-            throw new UnsupportedUserException(sprintf('Expected an instance of FOS\UserBundle\Model\User, but got "%s".', get_class($user)));
-        }
-
-        $userId = $this->accessor->getValue($user, $identifier);
-        if (null === $user = $this->userManager->findUserBy(array($identifier => $userId))) {
-            throw new UsernameNotFoundException(sprintf('User with ID "%d" could not be reloaded.', $userId));
-        }
-
-        return $user;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function supportsClass($class)
-    {
-        $userClass = $this->userManager->getClass();
-
-        return $userClass === $class || is_subclass_of($class, $userClass);
-    }
-
-    /**
-     * Gets the property for the response.
-     *
-     * @param UserResponseInterface $response
-     *
-     * @return string
-     *
-     * @throws \RuntimeException
-     */
-    protected function getProperty(UserResponseInterface $response)
-    {
-        $resourceOwnerName = $response->getResourceOwner()->getName();
-
-        if (!isset($this->properties[$resourceOwnerName])) {
-            throw new \RuntimeException(sprintf("No property defined for entity for resource owner '%s'.", $resourceOwnerName));
-        }
-
-        return $this->properties[$resourceOwnerName];
     }
 }
