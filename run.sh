26d22ff03b3cefdc5e25070c4a4c19df4eef13cd
po_taka <angel.koilov@gmail.com>
2019-05-13 19:51:28 +0300

    prod_setup_v3

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ set -e
 export WEB_UID=$UID
 
 docker build --tag=trackhub-web ./docker/web/
-
 docker-compose build
+
 if [ "$1" == "-p" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose-prod.yml build
+    docker-compose -p track -f docker-compose.yml -f docker-compose-prod.yml build
     docker-compose -p track -f docker-compose.yml -f docker-compose-prod.yml up
 else
     docker-compose -p track up
