10c48d68dd6834369769d024f0587238d3774aa7
Stefan Mavrodiev <stefan.mavrodiev@gmail.com>
2019-06-08 23:19:19 +0300

    Fix running containers with sudo

--- run.sh
+++ run.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-export WEB_UID=$UID
+# If the script is run with sudo, UID is 0. This is an issue when running
+# "usermod -u $WEB_UID www-data" in the web container.
+# In this case assign WEB_UID to 1000
+[[ $UID == 0 ]] && export WEB_UID=1000 || export WEB_UID=$UID
 
 docker build --tag=trackhub-web ./docker/web/
 docker-compose build
