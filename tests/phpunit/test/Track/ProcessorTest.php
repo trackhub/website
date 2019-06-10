fcde705f6cab631a205975d462814b4c4df2a77f
Stefan Mavrodiev <stefan@olimex.com>
2019-06-10 09:12:02 +0300

    Add additional unittests

--- tests/phpunit/test/Track/ProcessorTest.php
+++ tests/phpunit/test/Track/ProcessorTest.php
@@ -13,24 +13,110 @@ class ProcessorTest extends TestCase
 {
     public function testProcessorInvalidFile()
     {
-        $xml = "
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc 
-            accumsan leo quis arcu convallis faucibus. Mauris pharetra dolor 
-            faucibus, ultrices ex in, porttitor urna. Aliquam erat volutpat. 
-            Curabitur consectetur purus ex, at porta ex viverra in. Nullam 
+        $xml = '
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            accumsan leo quis arcu convallis faucibus. Mauris pharetra dolor
+            faucibus, ultrices ex in, porttitor urna. Aliquam erat volutpat.
+            Curabitur consectetur purus ex, at porta ex viverra in. Nullam
             rutrum sem quis lacus pellentesque rutrum. Quisque vitae arcu eros.
             Maecenas luctus enim at vehicula varius. In lacinia lorem vehicula
-            elementum fermentum. Aenean venenatis ac massa eu maximus. Lorem 
-            ipsum dolor sit amet, consectetur adipiscing elit. Quisque et 
+            elementum fermentum. Aenean venenatis ac massa eu maximus. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Quisque et
             varius eros.
-            ";
-
-        $user = new User();
+            ';
 
         $processor = new Processor();
+        $user = new User();
         $version = new Version($user);
 
         $this->expectException(Warning::class);
         $processor->process($xml, $version);
+    }
+
+    public function testProcessorInvalidFormat()
+    {
+        $xml = '
+            <dummy>
+            </dummy>
+        ';
+
+        $processor = new Processor();
+        $user = new User();
+        $version = new Version($user);
+
+        $this->expectException(\RuntimeException::class);
+        $processor->process($xml, $version);
+	}
+
+    public function testProcessorCorrupedFile()
+    {
+        /**
+         * Sample document from here:
+         * https://en.wikipedia.org/wiki/GPS_Exchange_Format#Sample_GPX_document
+         */
+        $xml = '
+        <gpx>
+          <trk>
+            <name>Example GPX Document</name>
+            <trkseg>
+              <trkpt lat="47.644548" lon="-122.326897">
+                <ele>4.46</ele>
+                <time>2009-10-17T18:37:26Z</time>
+              </trkpt>
+              <trkpt lat="47.644548" lon="-122.326897">
+                <ele>4.94</ele>
+                <time>2009-10-17T18:37:31Z</time>
+              </trkpt>
+              <trkpt lat="47.644548">
+                <ele>6.87</ele>
+                <time>2009-10-17T18:37:34Z</time>
+              </trkpt>
+            </trkseg>
+          </trk>
+        </gpx>
+        ';
+
+        $processor = new Processor();
+        $user = new User();
+        $version = new Version($user);
+
+        $processor->process($xml, $version);
+        $this->assertCount(2, $version->getPoints());
+    }
+
+    public function testProcessorCount()
+    {
+        /**
+         * Sample document from here:
+         * https://en.wikipedia.org/wiki/GPS_Exchange_Format#Sample_GPX_document
+         */
+        $xml = '
+        <gpx>
+          <trk>
+            <name>Example GPX Document</name>
+            <trkseg>
+              <trkpt lat="47.644548" lon="-122.326897">
+                <ele>4.46</ele>
+                <time>2009-10-17T18:37:26Z</time>
+              </trkpt>
+              <trkpt lat="47.644548" lon="-122.326897">
+                <ele>4.94</ele>
+                <time>2009-10-17T18:37:31Z</time>
+              </trkpt>
+              <trkpt lat="47.644548" lon="-122.326897">
+                <ele>6.87</ele>
+                <time>2009-10-17T18:37:34Z</time>
+              </trkpt>
+            </trkseg>
+          </trk>
+        </gpx>
+        ';
+
+        $processor = new Processor();
+        $user = new User();
+        $version = new Version($user);
+
+        $processor->process($xml, $version);
+        $this->assertCount(3, $version->getPoints());
     }
 }
