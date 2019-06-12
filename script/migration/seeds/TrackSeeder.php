a38568225e7287f6796d1f47f2be9c9cea193802
Stefan Mavrodiev <stefan.mavrodiev@gmail.com>
2019-06-13 00:35:17 +0300

    Update point/track/version seeders

--- script/migration/seeds/TrackSeeder.php
+++ script/migration/seeds/TrackSeeder.php
@@ -5,16 +5,41 @@ use Phinx\Seed\AbstractSeed;
 
 class TrackSeeder extends AbstractSeed
 {
-    /**
-     * Run Method.
-     *
-     * Write your database seeder using this method.
-     *
-     * More information on writing seeders is available here:
-     * http://docs.phinx.org/en/latest/seeding.html
-     */
+    const TYPE_CYCLING = 1;
+    const TYPE_HIKING = 2;
+
+    const VISIBILITY_PUBLIC = 0;
+    const VISIBILITY_UNLISTED = 1;
+
     public function run()
     {
 
+        if (!$this->hasTable('track'))
+            return;
+
+        $track = $this->table('track');
+
+        /* Disable constrains check during truncate */
+        $this->execute('SET FOREIGN_KEY_CHECKS = 0; ');
+        $track->truncate();
+        $this->execute('SET FOREIGN_KEY_CHECKS = 1; ');
+
+        $count = rand(1, 5);
+        for ($i = 0; $i < $count; $i++) {
+            $data = [
+                'id' => uniqid(),
+                'name' => 'Dummy track '.$i,
+                'last_check' => date('Y-m-d H:i:s',  strtotime(sprintf("+%d hours", $i))),
+                'point_north_east_lat' => 0,
+                'point_north_east_lng' => 0,
+                'point_south_west_lat' => 0,
+                'point_south_west_lng' => 0,
+                'type' => $this::TYPE_CYCLING,
+                'created_at' => date('Y-m-d H:i:s',  strtotime(sprintf("+%d hours", $i))),
+                'visibility' => $this::VISIBILITY_PUBLIC,
+            ];
+
+            $track->insert($data)->save();
+        }
     }
 }
