bd695c8a66aba69378796210dbf0318f80734a03
po_taka <angel.koilov@gmail.com>
2019-08-18 23:53:33 +0300

    file upload

---
+++ src/Entity/Track/Image.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity\Track;
+
+use App\Entity\Track;
+use App\Entity\User\User;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Image
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="UUID")
+     * @ORM\Column(name="id", type="guid")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Track", inversedBy="optimizedPoints")
+     */
+    private $track;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Track\Version", inversedBy="points")
+     */
+    private $version;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User\User")
+     */
+    private $sendBy;
+
+    /**
+     * @ORM\Column(type="string", nullable=false)
+     */
+    private $file;
+
+    public function __construct(string $file, User $sendBy, Track $track)
+    {
+        $this->file = $file;
+        $this->sendBy = $sendBy;
+        $this->track = $track;
+    }
+
+    public function setVersion(Version $version = null)
+    {
+        $this->version = $version;
+    }
+}
