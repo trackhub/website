abc61d25e60da5d2356e9a795ceb69f05d77631e
po_taka <angel.koilov@gmail.com>
2019-09-12 00:15:45 +0300

    seeders fix

---
+++ tests/travis/seeds_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+set -x
+
+bash ./run.sh  > /dev/null &
+timeout --foreground 360 bash -c "
+  until docker-compose -p track exec web curl localhost
+  do
+      echo 'Waiting for web server...'
+      sleep 3
+  done
+"
+
+echo "Web server is UP!"
+
+docker exec -it $(docker ps --format '{{.Names}}' | grep "web") /bin/bash -c "
+    cd script/migration && ./vendor/bin/phinx seed:run
+"
