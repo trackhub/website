f16007fff6a6626163d7028bd45c43c74256bb5e
Angel Koilov (po_taka) <angel.koilov@gmail.com>
2019-09-23 22:58:42 +0300

    fix image orientation (#138)

--- src/Image/ImageEdit.php
+++ src/Image/ImageEdit.php
@@ -12,7 +12,38 @@ class ImageEdit
         $image->setImageOrientation($image->getImageOrientation());
         $image->adaptiveResizeImage($maxW, $maxH, true);
 
+        $rotateAngle = $this->getRotateAngle($input);
+        if ($rotateAngle !== 0) {
+            $image->rotateImage('#FFFFFF', $rotateAngle);
+        }
+
+        $profiles = $image->getImageProfiles("icc", true);
+
+        // clear personal data stored in meta information
+        $image->stripImage();
+
+        // restore icc color profile
+        if (isset($profiles['icc'])) {
+            $image->profileImage("icc", $profiles['icc']);
+        }
+
         $fs = new Filesystem();
         $fs->dumpFile($output, $image);
+    }
+
+    public function getRotateAngle($imagePath): int
+    {
+        $exifData = exif_read_data($imagePath);
+
+        if (isset($exifData['Orientation'])) {
+            switch ($exifData['Orientation']) {
+                case 6:
+                    return 90;
+                case 8:
+                    return 270;
+            }
+        }
+
+        return 0;
     }
 }
