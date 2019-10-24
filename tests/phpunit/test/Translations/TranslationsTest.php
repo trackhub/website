e7a45311b64ac1068602f700972174e8b9dd686b
Stefan Mavrodiev <stefan.mavrodiev@gmail.com>
2019-10-24 23:32:57 +0300

    Remove duplicate keys test and unused code

--- tests/phpunit/test/Translations/TranslationsTest.php
+++ tests/phpunit/test/Translations/TranslationsTest.php
@@ -8,25 +8,10 @@ class TranslationsTest extends TestCase
 {
 
     private const FILES = [
-      'messages.bg.php',
-      'messages.en.php',
-      'validators.bg.php'
+        'messages.bg.php',
+        'messages.en.php',
+        'validators.bg.php'
     ];
-
-    public function load(string $file): array
-    {
-        $translations = require 'translations/' . $file;
-        /**
-         * $duplicates variable is loaded from the translation file
-         * @var array $duplicates
-         */
-        $allowed = isset($duplicates) ? $duplicates : [];
-
-        return [
-            'translations' => $translations,
-            'allowed' => $allowed,
-        ];
-    }
 
     /**
      * For readability, translation messages should be sorted alphabetically.
@@ -35,24 +20,11 @@ class TranslationsTest extends TestCase
     public function testArrangement(): void
     {
         foreach (self::FILES as $file) {
-            $trans = $this->load($file);
+            $trans = require 'translations/' . $file;
 
-            $sorted = $trans['translations'];
+            $sorted = $trans;
             ksort($sorted);
-            $this->assertSame($sorted, $trans['translations'], "The file '" . $file . "' is not sorted alphabetically");
-        }
-    }
-
-    /**
-     * Test for duplicate keys
-     */
-    public function testDuplicateKeys(): void
-    {
-        foreach (self::FILES as $file) {
-            $trans = $this->load($file);
-
-            $output = shell_exec("cat translations/" . $file . " | grep '=>' | awk -F'=>' '{print $1}' | uniq -d | xargs | tr -d '\\n'");
-            $this->assertNull($output, "There is duplicate keys in '" . $file . "'");
+            $this->assertSame($sorted, $trans, "The file '" . $file . "' is not sorted alphabetically");
         }
     }
 }
