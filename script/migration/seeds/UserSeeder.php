177f10408407103d6dffb19fb256c309ab18e7a3
Stefan Mavrodiev <stefan.mavrodiev@gmail.com>
2020-01-28 20:52:02 +0200

    Fix user seeders

--- script/migration/seeds/UserSeeder.php
+++ script/migration/seeds/UserSeeder.php
@@ -21,16 +21,13 @@ class UserSeeder extends AbstractSeed
         $data = [];
 
         for ($i = 0; $i < self::USER_COUNT; $i++) {
-            $username = $faker->firstName . ' ' . $faker->lastName;
+            $nickname = $faker->firstName . ' ' . $faker->lastName;
             $email = $faker->email;
-            $this->getOutput()->writeln("Generating: {$username}", OutputInterface::VERBOSITY_VERY_VERBOSE);
+            $this->getOutput()->writeln("Generating: {$nickname}", OutputInterface::VERBOSITY_VERY_VERBOSE);
             $data[] = [
-                'username' => $username,
-                'username_canonical' => mb_convert_case($username, MB_CASE_LOWER, "UTF-8"),
+                'nickname' => $nickname,
                 'email' => $email,
-                'email_canonical' => mb_convert_case($email, MB_CASE_LOWER, "UTF-8"),
                 'enabled' => true,
-                'password' => sha1(base64_encode(random_bytes(30))),
                 'facebook_id' => random_int(1e17, 1e18),
                 'roles' => "a:0:{}",
                 'terms_accepted' => date('Y-m-d H:i:s')
