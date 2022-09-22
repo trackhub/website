f3fb520ed3029027938a4cd97b6372bf15d4b1c2
po_taka <angel.koilov@gmail.com>
2022-09-22 18:17:52 +0300

    leaflet to yarn

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,15 +13,27 @@ require('bootstrap');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
-
+import * as L from "leaflet"
 window.jQuery = require('jquery');
 window.chartJs = require('chart.js');
 window.lighBox = require('ekko-lightbox/dist/ekko-lightbox.js');
+require('leaflet.markercluster')
+window.MapControlFullScreen =  require('./map/control/fullScreen')
+
+import { GestureHandling } from "leaflet-gesture-handling";
+L.Map.addInitHook("addHandler", "gestureHandling", GestureHandling);
+
+// see https://github.com/PaulLeCam/react-leaflet/issues/255#issuecomment-269750542
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 window.AppTrack = require('./components/appTrack.js');
 window.AppPlace = require('./map/appPoint.js');
 window.StarRating = require('./components/starRating');
 
-window.MapControlFullScreen =  require('./map/control/fullScreen')
 window.jsUpload = require('./components/fileUpload');
 window.bsCustomFileInput = require('bs-custom-file-input');
