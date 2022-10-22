9db4033325d0e92b556edc47d66f6907b9872147
po_taka <angel.koilov@gmail.com>
2022-10-22 11:24:41 +0300

    gh disable

--- assets/js/places.js
+++ assets/js/places.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const map = L.map('map', {
-            gestureHandling: true,
+            gestureHandling: false,
         }
     );
 
